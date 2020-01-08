08b21de565a07b1edf197049b6284eb3a4a1db33
mark <markhinojosa1212@gmail.com>
2020-01-07 19:36:13 -0600

    fixed security issue and added auto scroll for messages

--- src/components/SendMessageForm.js
+++ src/components/SendMessageForm.js
@@ -1,45 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class SendMessageForm extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            text: ''
-        }
-        this.onSubmit = this.onSubmit.bind(this)
-        this.onChange = this.onChange.bind(this)
-    }
-    componentDidMount() {
-        console.log(this.props)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  componentDidMount() {
+    console.log(this.props);
+  }
 
+  onSubmit(e) {
+    e.preventDefault();
+    this.props.onSubmit(this.state.text);
+    this.setState({ text: "" });
+  }
 
-    onSubmit(e) {
-        e.preventDefault()
-        this.props.onSubmit(this.state.text)
-        this.setState({ text: '' })
+  onChange(e) {
+    this.setState({ text: e.target.value });
+    if (this.props.onChange) {
+      this.props.onChange();
     }
+  }
 
-    onChange(e) {
-        this.setState({ text: e.target.value })
-        if(this.props.onChange) {
-            this.props.onChange()
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
-                    <input
-                    type="text"
-                    placeholder="Type Message Here"
-                    onChange={this.onChange}
-                    value={this.state.text}
-                    style={{width: '100%'}}
-                    />
-                </form>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={this.onSubmit}
+          style={{ display: "flex", marginBottom: "1%" }}
+        >
+          <input
+            type="text"
+            placeholder="Type Message Here"
+            onChange={this.onChange}
+            value={this.state.text}
+            style={{ width: "100%" }}
+          />
+        </form>
+      </div>
+    );
+  }
 }
